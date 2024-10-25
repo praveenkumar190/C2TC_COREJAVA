cfc6b53408d25084d907a49c0817bad5b303faaf
91948 <91948@praveen007>
2024-10-25 10:32:12 +0530

    Initial Commit

---
+++ C2TC_PRAVEENKUMAR/src/com/tns/ifet/day1/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+	}
+
+}
