afaf82b2f44953ad0dddbb382799cd9b5d0a4755
91948 <91948@praveen007>
2024-11-04 15:48:33 +0530

    day3 commit

---
+++ C2TC_PRAVEENKUMAR/src/com/tns/ifet/day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.day3.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Arun");
+		obj.setAge(20);
+		System.out.println(obj);
+	}
+
+}
