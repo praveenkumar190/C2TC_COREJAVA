154190aeb5d8bf3f33291d8977056796839c1875
91948 <91948@praveen007>
2024-11-12 20:49:28 +0530

    day 11 commit

---
+++ C2TC_PRAVEENKUMAR/src/com/tns/ifet/day11/fnallyblock/v1/Demo.java
@@ -0,0 +1,10 @@
+//Program without exception handling
+package com.tns.ifet.day11.fnallyblock.v1;
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
